17b02a1946523d3f2ca6d5867f96bf504e631592
Shayan <expertshayan@gmail.com>
2022-12-11 11:45:35 +0500

    printing patterns

---
+++ 10_Printing_Patterns.py
@@ -0,0 +1,13 @@
+# # pattern printing
+# def aPattern():
+#     print("*****")
+#     print("*****")
+#     print("*****")
+#     print("*****")
+#     print("*****")
+    
+# def bPattern():
+#     pass
+
+# def cPattern():
+#     pass
